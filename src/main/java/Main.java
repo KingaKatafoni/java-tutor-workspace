c171534d452dafeb77dc428d1889b04462bf8aa1
Kinga <kiniafruit@gmail.com>
2026-04-15 21:52:38 +0200

    Drugi commit zmienia syso.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Kinga! Dzien 2 — Maven dziala!");
+        System.out.println("Drugi commit w życiu :) ");
     }
 }
