a7d241a5f4751392a1a86ab4cb2205d515cfd3b3
Kinga <kiniafruit@gmail.com>
2026-04-15 21:45:41 +0200

    Initial commit: setup Maven project

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Kinga! Dzien 2 — Maven dziala!");
+    }
+}
---
+++ src/main/java/pl/kinga/Main.java
@@ -0,0 +1,9 @@
+package pl.kinga;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Kinga! Dzien 2 — Maven dziala!");
+    }
+}
