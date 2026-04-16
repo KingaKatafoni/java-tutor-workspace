ed648f478098b81c15ccf4762906628336f88bce
Kinga <kiniafruit@gmail.com>
2026-04-16 14:09:05 +0200

    Przeniesienie Main do pakietu pl.kinga

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Kinga! Dzien 2 — Maven dziala!");
-        System.out.println("Drugi commit w życiu :) ");
-    }
-}
--- src/main/java/pl/kinga/Main.java
+++ src/main/java/pl/kinga/Main.java
@@ -1,9 +1,8 @@
 package pl.kinga;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Kinga! Dzien 2 — Maven dziala!");
+        System.out.println("Drugi commit w życiu :) ");
     }
 }
