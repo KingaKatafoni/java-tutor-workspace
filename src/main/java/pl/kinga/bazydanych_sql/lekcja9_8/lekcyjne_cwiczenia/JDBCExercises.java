4f7d2dada16198aa0e527f74d8c6a5fa8231ae35
Kinga <kiniafruit@gmail.com>
2026-08-03 23:19:25 +0200

    JDBC

---
+++ src/main/java/pl/kinga/bazydanych_sql/lekcja9_8/lekcyjne_cwiczenia/JDBCExercises.java
@@ -0,0 +1,21 @@
+package pl.kinga.bazydanych_sql.lekcja9_8.lekcyjne_cwiczenia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExercises {
+    String url = "jdbc:h2:mem:testdb";
+    String user = "sa";
+    String password = "";
+
+   Connection connection;
+
+   public JDBCExercises(){
+
+   }
+
+
+
+}
